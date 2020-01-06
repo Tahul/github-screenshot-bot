ee1a79c7e0bca1b28a19125f409c1e9bd7df856e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-01-06 13:38:02 +0100

    :sparkles: (video) removed videoshow, switched to raw ffmpeg instead

--- video.js
+++ video.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 const sharp = require("sharp")
-const videoshow = require("videoshow")
+const execSync = require("child_process").execSync
 
 const getDateFromFilename = date => {
   const dateParts = date.split("-")
@@ -18,28 +18,25 @@ const run = async () => {
   let filesCropped = 0
 
   for (const rawFile of rawFiles) {
-    await sharp(rawFile)
-      .extract({ width: 1920, height: 900, left: 0, top: 300 })
-      .toFile("tmp/" + rawFile.replace("screenshots/screenshot-", ""))
+    try {
+      /* await sharp(rawFile)
+        .extract({ width: 1920, height: 900, left: 0, top: 300 })
+        .toFile("tmp/" + rawFile.replace("screenshots/screenshot-", "")) */
+    } catch (e) {
+      console.log("Cannot crop " + rawFile)
+    }
 
     filesCropped = filesCropped + 1
 
     console.log(filesCropped + "/" + rawFiles.length + " files cropped...")
   }
 
-  const videoOptions = {
-    fps: 30,
-    loop: 0.125,
-    transition: false,
-    videoBitrate: 1024,
-    videoCodex: "libx264",
-    size: "1280x?",
-    format: "mp4"
-  }
+  const list = await fs.writeFileSync("tmp/list.txt", "")
 
   const croppedFiles = fs
     .readdirSync("./tmp/")
     .filter(item => item !== ".gitignore")
+    .filter(item => item !== "list.txt")
     .filter(item => item !== "video.mp4")
     .map(item => "tmp/" + item)
     .sort((a, b) => {
@@ -58,22 +55,31 @@ const run = async () => {
     console.log("Creating the video...")
     const videoPath = "./screenshots/video.mp4"
 
-    await videoshow(croppedFiles, videoOptions)
-      .save(videoPath)
-      .on("error", error => console.log(`Encoding Error: ${error.message}`))
-      .on("exit", () => console.log("Video recorder exited"))
-      .on("close", () => console.log("Video recorder closed"))
-      .on("end", async () => {
-        console.log("Cleaning up tmp files")
-        for (const file of croppedFiles) {
-          try {
-            await fs.unlinkSync(file)
-          } catch (e) {
-            console.log("Cannot unlink " + file)
-          }
-        }
-        console.log("Video created: " + videoPath)
-      })
+    for (const file of croppedFiles) {
+      try {
+        await fs.appendFileSync(
+          "tmp/list.txt",
+          "\nfile '" + file.replace("tmp/", "") + "'"
+        )
+        await fs.appendFileSync("tmp/list.txt", "\nduration 0.10")
+      } catch (e) {
+        console.log("Cannot write " + file)
+      }
+    }
+
+    await execSync(
+      "ffmpeg -y -f concat -i tmp/list.txt -codec libx264 screenshots/video.mp4 "
+    )
+
+    console.log("Cleaning up tmp files")
+
+    for (const file of fs
+      .readdirSync("./tmp/")
+      .filter(item => item !== ".gitignore")) {
+      await fs.unlinkSync(file)
+    }
+
+    console.log("Video created: " + videoPath)
   } catch (e) {
     console.log(e)
   }
